0b0dda419e8b5e3bcedacee16a858b8a1905a7f3
nguyenhuunha <nguyenhuunha@gmail.com>
2022-06-13 23:28:14 +0700

    main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DataEmitterComponent } from './data-emitter/data-emitter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DataEmitterComponent},
+  { path: 'data', component: DataEmitterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
